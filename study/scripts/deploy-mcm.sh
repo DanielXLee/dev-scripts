7393522aff9e66660ba6d358c5d3aae00bd6b60c
Xin Li <lixin8611@gmail.com>
2021-06-10 11:40:20 +0800

    Update mcm install script

--- study/scripts/deploy-mcm.sh
+++ study/scripts/deploy-mcm.sh
@@ -27,33 +27,35 @@ install_kind() {
   mv ./kind /usr/local/bin/kind
 }
 
-create_clusters() {
+create_kind_clusters() {
   echo "Creating clusters with kind ..."
-  create_cluster $hub_cluster_name
+  create_kind_cluster $hub_cluster_name
   for NAME in ${managed_cluster_names[@]}
   do
-    create_cluster $NAME
+    create_kind_cluster $NAME
   done
 }
 
-create_cluster() {
-  NAME=$1
-  echo "Creating cluster $NAME ..."
-  kind create cluster --name $NAME
+create_kind_cluster() {
+  kind create cluster --name $1
 }
 
 deploy_mcm() {
+  deploy_hub_cluster
+  for NAME in ${managed_cluster_names[@]}
+  do
+    deploy_managed_cluster $NAME
+  done
+}
+
+deploy_hub_cluster() {
   echo "Creating hub cluster"
   kind get kubeconfig --name ${hub_cluster_name} --internal > ~/${hub_cluster_name}-kubeconfig
   kubectl config use-context kind-${hub_cluster_name}
   make deploy-hub
-  for NAME in ${managed_cluster_names[@]}
-  do
-    create_managed_cluster $NAME
-  done
 }
 
-create_managed_cluster() {
+deploy_managed_cluster() {
   NAME=$1
   echo "Creating managed cluster $NAME"
   kind get kubeconfig --name ${NAME} --internal > ~/${NAME}-kubeconfig
@@ -76,7 +78,7 @@ approve_csr() {
   kubectl config use-context kind-${hub_cluster_name}
   echo "Approve CSR with command: kubectl certificate approve $(kubectl get csr | grep ${NAME} | awk '{print $1}')"
   kubectl certificate approve $(kubectl get csr | grep ${NAME} | awk '{print $1}')
-  echo "Accept cluster with command: kubectl patch managedcluster ${NAME} -p='{"spec":{"hubAcceptsClient":true}}' --type=merge"
+  echo "Accept cluster with command: kubectl patch managedcluster ${NAME} -p='{\"spec\":{\"hubAcceptsClient\":true}}' --type=merge"
   kubectl patch managedcluster ${NAME} -p='{"spec":{"hubAcceptsClient":true}}' --type=merge
 }
 
@@ -98,7 +100,7 @@ deploy_application_operators() {
 
 deploy_application() {
   kubectl config use-context kind-${hub_cluster_name}
-  kubectl apply -f examples/helmrepo-hub-channel
+  kubectl apply -f deployment.yaml
 
   echo "Checking deployed resources"
   sleep 60
@@ -108,6 +110,15 @@ deploy_application() {
   do
     echo "Fetch resources on $NAME, run command: kubectl --context kind-${NAME} -n default get deploy,po"
     kubectl --context kind-${NAME} -n default get deploy,po
+  done
+}
+
+add_label_for_managed_clusters() {
+  for NAME in ${managed_cluster_names[@]}
+  do
+    [[ "$NAME" == "cluster1" ]] && continue
+    echo "Accept cluster with command: kubectl patch managedcluster ${NAME} -p='{\"metadata\":{\"labels\":{\"environment\": \"Dev\"}}}' --type=merge"
+    kubectl patch managedcluster ${NAME} -p='{"metadata":{"labels":{"environment": "Dev"}}}' --type=merge
   done
 }
 
@@ -127,6 +138,7 @@ cleanup_all_clusters () {
 }
 
 clone_code() {
+  git clone https://github.com/kubernetes-app/mcm-example-apps.git
   git clone https://github.com/kubernetes-app/registration-operator.git
   git clone https://github.com/open-cluster-management/multicloud-operators-subscription.git
 }
@@ -140,15 +152,29 @@ if [[ "X$1" == "X" || "$1" == "install" ]]; then
   if [[ "X$KIND" == "X" ]]; then
     install_kind
   fi
-  create_clusters
+  echo "Step 1: Clone source code and prepare kind clusters"
   clone_code
-  (cd registration-operator && deploy_mcm && approve_csrs)
-  (cd multicloud-operators-subscription && deploy_application_operators)
-  (cd multicloud-operators-subscription && deploy_application)
+  create_kind_clusters
+
+  echo "Step 2: Deploy hub and managed components base on kind clusters"
+  pushd registration-operator
+  deploy_mcm
+  sleep 100
+  approve_csrs
+  add_label_for_managed_clusters
+  popd
+
+  echo "Step 3: Deploy open cluster manager application lifecycle components"
+  pushd multicloud-operators-subscription
+  deploy_application_operators
+  popd
+  pushd mcm-example-apps
+  deploy_application
+  popd
 elif [[ "$1" == "add" ]]; then
   pushd registration-operator
-  create_cluster $2
-  create_managed_cluster $2
+  create_kind_cluster $2
+  deploy_managed_cluster $2
   approve_csr $2
   popd
 elif [[ "$1" == "delete" ]]; then
@@ -162,4 +188,6 @@ elif [[ "$1" == "app" ]]; then
   popd
 elif [[ "$1" == "cleanup" ]]; then
   cleanup_all_clusters
+elif [[ "$1" == "label" ]]; then
+  add_label_for_managed_clusters
 fi
